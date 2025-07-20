6cf44caab190cdc803f1444bdd0890eb2e0e0cf5
baturaykayaturk <bkayaturk@guardme.org>
2025-07-20 11:32:19 -0400

    refactor: main.tf

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -52,10 +52,12 @@ module "eks" {
 ########################  EKS DESCRIPTOR  ########################
 data "aws_eks_cluster" "eks" {
   name = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "eks" {
   name = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
 ########################  KUBERNETES PROVIDER  ###################
