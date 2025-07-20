e2d33a7e7b68b5dc24e5a620ed34558ea147f3a7
baturaykayaturk <bkayaturk@guardme.org>
2025-07-20 12:20:18 -0400

    refactor: main.tf

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -2,11 +2,23 @@
 terraform {
   required_version = ">= 1.5"
 
-  required_providers = {
-    aws        = { source = "hashicorp/aws",        version = "~> 5.0" }
-    helm       = { source = "hashicorp/helm",       version = "~> 2.11" }
-    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.31" }
-    http       = { source = "hashicorp/http",       version = "~> 3.2" }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.31"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.2"
+    }
   }
 }
 
