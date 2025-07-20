21a124efeec63d45de2ddbc4abab527736a14220
baturaykayaturk <bkayaturk@guardme.org>
2025-07-19 23:43:09 -0400

    refactor: main.tf, variables.tf

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -1,3 +1,13 @@
+#################### Providers ####################
+terraform {
+  required_version = ">= 1.5"
+  required_providers {
+    aws    = "~> 5.0"
+    helm   = "~> 2.11"
+    kubernetes = "~> 2.31"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
@@ -9,13 +19,14 @@ module "vpc" {
 
   name = "eks-demo-vpc"
   cidr = "10.0.0.0/16"
-  azs  = ["us-east-1a", "us-east-1b"]
+  azs  = ["ca-central-1a", "ca-central-1b"]
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
+  enable_dns_hostnames = true
 }
 
 ################################  EKS  ################################
@@ -23,7 +34,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.37.2"
 
-  cluster_name    = "hiive-demo"
+  cluster_name    = "hiive-eks-demo"
   cluster_version = "1.29"
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
@@ -45,15 +56,72 @@ module "eks" {
   }
 }
 
-################################  ALB ADD‑ON  #########################
-module "addons" {
-  source  = "terraform-aws-modules/eks/aws//modules/addons"
-  version = "20.37.2"
-  cluster_name = module.eks.cluster_name
-  region       = var.aws_region
+################ IAM for LB Controller ############
+data "aws_iam_policy_document" "lb_ctlr_assume" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [module.eks.oidc_provider_arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "${module.eks.oidc_provider}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
 
-  addon_config = {
-    aws-load-balancer-controller = { most_recent = true }
+resource "aws_iam_role" "lb_ctlr" {
+  name               = "AWSLoadBalancerControllerRole"
+  assume_role_policy = data.aws_iam_policy_document.lb_ctlr_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "lb_ctlr_attach" {
+  role       = aws_iam_role.lb_ctlr.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSLoadBalancerControllerIAMPolicy"
+}
+
+#################### Helm – AWS LB Controller #####
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = module.eks.cluster_token
+  }
+}
+
+resource "helm_release" "lb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.7.1"
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "region"
+    value = var.aws_region
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.lb_ctlr.arn
   }
 }
 
--- envs/prod/variables.tf
+++ envs/prod/variables.tf
@@ -1,4 +1,4 @@
-variable "aws_region"      { default = "us-east-1" }
+variable "aws_region"      { default = "ca-central-1" }
 variable "aws_role_arn"    { type = string }
 variable "tf_state_bucket" { type = string }
 variable "tf_state_table"  { type = string }
