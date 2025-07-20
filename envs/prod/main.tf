4910ff267d73a4dbb426e68bac3c5d4648992a32
baturaykayaturk <bkayaturk@guardme.org>
2025-07-20 13:27:16 -0400

    refactor: main.tf

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -66,7 +66,7 @@ module "eks" {
     }
   }
 
-  enable_cluster_creator_admin_permissions = true
+  enable_cluster_creator_admin_permissions = false
 
   authentication_mode = "API_AND_CONFIG_MAP"
 }
