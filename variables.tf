7435628d2440a8d9cb27acaad3fa9d55d46a0e5a
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2025-07-19 22:11:00 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "aws_region"      { default = "us-east-1" }
+variable "aws_role_arn"    { type = string }
+variable "tf_state_bucket" { type = string }
+variable "tf_state_table"  { type = string }
